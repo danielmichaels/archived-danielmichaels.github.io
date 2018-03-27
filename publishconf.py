0d86bdd097b5e880c0c64a631653f9fe12fff186
Dan <dans.address@outlook.com>
2018-03-27 15:34:40 +0000

    Add SITEURL: https://danielms.site

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://danielmichaels.github.io'
+SITEURL = 'https://danielms.site'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
